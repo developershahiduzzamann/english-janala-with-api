e0780db252d507dcfc8cd598a97c5ede9b28378b
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-12-31 21:34:32 +0600

    added the all section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,39 @@
-// document.getElementById("display-header").style.display ="none"
-// document.getElementById("vocabularies").style.display ="none"
-// document.getElementById("frequently").style.display ="none"
-// document.getElementById("btn-get-start").addEventListener("click",function (event){
-//     event.preventDefault();
-//     const inputName = document.getElementById("input-name").value
-//     const inputPassword = document.getElementById("input-password").value
-//     const pin = parseInt(inputPassword)
+document.getElementById("display-header").style.display ="none"
+document.getElementById("vocabularies").style.display ="none"
+document.getElementById("frequently").style.display ="none"
+document.getElementById("btn-get-start").addEventListener("click",function (event){
+    event.preventDefault();
+    const inputName = document.getElementById("input-name").value
+    const inputPassword = document.getElementById("input-password").value
+    const pin = parseInt(inputPassword)
 
-//     if(inputName){
-//         if(pin === 123456){
-//             document.getElementById("display-header").style.display ="block"
-//             document.getElementById("vocabularies").style.display ="block"
-//             document.getElementById("frequently").style.display ="block"
-//         }
-//         else{
-//             alert("Pin Not Vali")
-//         }
-//     }
-//     else{
-//         alert("Input Your Name")
-//     }
+    if(inputName){
+        if(pin === 123456){
+            document.getElementById("display-header").style.display ="block"
+            document.getElementById("vocabularies").style.display ="block"
+            document.getElementById("frequently").style.display ="block"
+        }
+        else{
+            alert("Pin Not Vali")
+        }
+    }
+    else{
+        alert("Input Your Name")
+    }
     
-// })
+})
 
-// document.getElementById("log-Out").addEventListener("click",function(){
-//     document.getElementById("vocabularies").style.display ="none"
-//     document.getElementById("frequently").style.display ="none"
-//     document.getElementById("display-header").style.display ="none"
-// })
-// document.getElementById("btn-faq").addEventListener("click", function() {
-//     document.getElementById("frequently").scrollIntoView({ behavior: "smooth" });
-// });
-// document.getElementById("btn-learn").addEventListener("click", function() {
-//     document.getElementById("vocabularies").scrollIntoView({ behavior: "smooth" });
-// });
+document.getElementById("log-Out").addEventListener("click",function(){
+    document.getElementById("vocabularies").style.display ="none"
+    document.getElementById("frequently").style.display ="none"
+    document.getElementById("display-header").style.display ="none"
+})
+document.getElementById("btn-faq").addEventListener("click", function() {
+    document.getElementById("frequently").scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("btn-learn").addEventListener("click", function() {
+    document.getElementById("vocabularies").scrollIntoView({ behavior: "smooth" });
+});
 
 
 
